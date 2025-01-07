950944bd3ce8bb663b7ce66a728cdf26f3557a20
yogeshdnumb <dyogesh.official@gmail.com>
2025-01-07 23:15:01 +0530

    added more rss feeds

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,16 @@ const parser = new Parser();
 
 const urls = [
   ["https://news.ycombinator.com/rss", "hackerNews"],
-  ["https://feeds.arstechnica.com/arstechnica/index/", "ArtsTechnica"],
+  ["https://feeds.arstechnica.com/arstechnica/index/", "artsTechnica"],
+  ["https://www.theverge.com/rss/index.xml", "theVerge"],
+  ["https://www.theregister.com/headlines.atom", "theRegister"],
+  ["https://finance.yahoo.com/news/rssindex", "yahooFinance"],
+  [
+    "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=10001147",
+    "cnbcBusiness",
+  ],
+  ["http://rss.sciam.com/basic-science", "scientificAmerican"],
+  ["https://www.quantamagazine.org/feed/", "quantaMagazine"],
 ];
 
 async function rssToJson(url: string) {
